280fc52ccc435bf28c026d4caf8eebde7fce7ca4
GrzeCHu-a11y <ggonczowski@zsp5kozuchow.pl>
2023-12-14 20:53:17 +0100

    basic structure off project card and images directory change

--- src/components/templates/Home.jsx
+++ src/components/templates/Home.jsx
@@ -15,34 +15,34 @@ const Home = () => {
       <h3 className={styles.h3}>Techstack: </h3>
       <div className={styles.techstackContainer}>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-react-a-javascript-library-for-building-user-interfaces-96.png" alt="" />
+          <img className={styles.img} src="/icons8-react-a-javascript-library-for-building-user-interfaces-96.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-javascript-96.png" alt="" />
+          <img className={styles.img} src="/icons8-javascript-96.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-css-144.png" alt="" />
+          <img className={styles.img} src="/icons8-css-144.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-typescript-96.png" alt="" />
+          <img className={styles.img} src="/icons8-typescript-96.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-node-js-96.png" alt="" />
+          <img className={styles.img} src="/icons8-node-js-96.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/php.png" alt="" />
+          <img className={styles.img} src="/php.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/next-js.256x256.png" alt="" />
+          <img className={styles.img} src="/next-js.256x256.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-mysql-96.png" alt="" />
+          <img className={styles.img} src="/icons8-mysql-96.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-graphql-96.png" alt="" />
+          <img className={styles.img} src="/icons8-graphql-96.png" alt="" />
         </div>
         <div className={styles.item}>
-          <img className={styles.img} src="/public/icons8-wordpress-96.png" alt="" />
+          <img className={styles.img} src="/icons8-wordpress-96.png" alt="" />
         </div>
       </div>
     </section>
